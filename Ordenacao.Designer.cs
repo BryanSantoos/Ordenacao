6d2c514608aa0c21528094a5ddcf4d09798d4dac
Bryan Santos <49533832+BryanSantoos@users.noreply.github.com>
2019-04-16 19:26:52 -0300

    Add files via upload

---
+++ Ordenacao.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmOrdenacao
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblb = new System.Windows.Forms.Label();
+            this.lblc = new System.Windows.Forms.Label();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(128, 42);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(59, 20);
+            this.txtA.TabIndex = 0;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(128, 69);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(59, 20);
+            this.txtB.TabIndex = 1;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(128, 96);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(59, 20);
+            this.txtC.TabIndex = 2;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.Location = new System.Drawing.Point(109, 44);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(17, 16);
+            this.lblA.TabIndex = 3;
+            this.lblA.Text = "A";
+            this.lblA.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblb
+            // 
+            this.lblb.AutoSize = true;
+            this.lblb.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblb.Location = new System.Drawing.Point(109, 71);
+            this.lblb.Name = "lblb";
+            this.lblb.Size = new System.Drawing.Size(17, 16);
+            this.lblb.TabIndex = 4;
+            this.lblb.Text = "B";
+            // 
+            // lblc
+            // 
+            this.lblc.AutoSize = true;
+            this.lblc.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblc.Location = new System.Drawing.Point(109, 98);
+            this.lblc.Name = "lblc";
+            this.lblc.Size = new System.Drawing.Size(17, 16);
+            this.lblc.TabIndex = 5;
+            this.lblc.Text = "C";
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.BackColor = System.Drawing.SystemColors.Control;
+            this.btnOrdenar.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrdenar.Location = new System.Drawing.Point(112, 122);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(75, 23);
+            this.btnOrdenar.TabIndex = 6;
+            this.btnOrdenar.Text = "ORDENAR";
+            this.btnOrdenar.UseVisualStyleBackColor = false;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.SystemColors.Control;
+            this.btnLimpar.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(112, 151);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // frmOrdenacao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.lblc);
+            this.Controls.Add(this.lblb);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Name = "frmOrdenacao";
+            this.Text = "Ordenação";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblb;
+        private System.Windows.Forms.Label lblc;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ Ordenacao.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmOrdenacao : Form
+    {
+        public frmOrdenacao()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            //Possibilidades:
+            /* A B C
+             * A C B
+             * B A C
+             * B C A
+             * C A B
+             * C B A
+             */
+            //Declarações das variáveis
+            int A, B, C;
+            //Entrada de dados 
+            A = Convert.ToInt32(txtA.Text);
+            B = Convert.ToInt32(txtB.Text);
+            C = Convert.ToInt32(txtC.Text);
+            if (A <= B && B <= C)
+            {
+                MessageBox.Show(A + " - " + B + " - " + C);
+            }
+            else if (A <= C && C <= B)
+            {
+                MessageBox.Show(A + " - " + C + " - " + B);
+            }
+            else if (B <= A && A <= C)
+            {
+                MessageBox.Show(B + " - " + A + " - " + C);
+            }
+            else if (B <= C && C <= A)
+            {
+                MessageBox.Show(B + " - " + C + " - " + A);
+            }
+            else if (C <= A && A <= B)
+            {
+                MessageBox.Show(C + " - " + A + " - " + B);
+            }
+            else         
+            {                             
+                MessageBox.Show(C + " - " + B + " - " + A);
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtA.Text = "";
+            txtB.Text = "";
+            txtC.Text = "";
+        }
+    }
+}
